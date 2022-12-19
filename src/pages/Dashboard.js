98d6c495b5b58850376fdd1ad5e16b98020950e4
eyrooonnn <espinosa.aaron45@gmail.com>
2022-12-20 01:02:19 +0800

    shet nasira yung nav

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,45 +1,11 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
 import { NavBar } from '../components'
-import { GiHamburgerMenu } from "react-icons/gi";
 
 const Dashboard = () => {
 
-  const [navActive, setNavActive] = useState(false)
-
-  useEffect(() => {
-    console.log(navActive)
-  }, [navActive])
-
-  const navClick = () => {
-    setNavActive(current => !current)
-  }
-
   return (
     <>
-      <div className='w-full h-screen flex flex-row font-space'>
-        {/* NAVIGATION */}
-        <NavBar />
-
-        {/* FEED */}
-        <div className='bg-wht w-full h-full'>
-          <div className='border-b-2 border-blk h-16 lg:h-28 flex'>
-
-            {/* NAV BUTTON: FOR MOBILE */}
-            <div className='h-16 w-16 bg-blk lg:hidden'>
-              <div className='text-wht flex items-center justify-center w-full h-full' onClick={navClick}>
-                <GiHamburgerMenu className='text-3xl' />
-              </div>
-            </div>
-
-            {/* GREETING */}
-            <div className='my-auto pl-3 lg:pl-10 text-2xl lg:text-5xl font-bold select-none'>Good day, AJ</div>
-          </div>
-          <div>
-            <div></div>
-          </div>
-        </div>
-      </div>
+      <NavBar />
     </>
   )
 }
