842e808c781f563baa8060cdb77d14e2918ae390
eyrooonnn <espinosa.aaron45@gmail.com>
2022-12-20 00:41:48 +0800

    added side navigation bar

---
+++ src/assets/index.js
@@ -0,0 +1,15 @@
+import appointment from "./appointment.svg"
+import complaint from "./complaint.svg"
+import dashboard from "./dashboard.svg"
+import faq from "./faq.svg"
+import marketplace from "./marketplace.svg"
+import news from "./news.svg"
+
+export {
+    appointment,
+    complaint,
+    dashboard,
+    faq,
+    marketplace,
+    news
+}
