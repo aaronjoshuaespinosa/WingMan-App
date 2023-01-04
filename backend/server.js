e6bd358af12ad2fd260d18fdc5abdfcae2b6b158
eyrooonnn <espinosa.aaron45@gmail.com>
2023-01-04 20:57:14 +0800

    init backend

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const app = express();
+
+app.listen(4000, () => {
+    console.log('listening on port 4000.');
+});
+
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to the App.'});
+});
