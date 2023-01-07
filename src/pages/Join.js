e772fe2c93ee75a7a5fae98e889d7389523bee68
AJ Espinosa <espinosa.aaron45@gmail.com>
2023-01-07 23:48:38 +0800

    Login and register validation fix

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -148,7 +148,7 @@ const Join = () => {
                                 })}
                                 
                                 <button className='bg-orng w-full h-[40px] text-center text-blk flex items-center justify-center text-sm font-bold rounded-[2px] border-[2px] border-blk select-none cursor-pointer mb-2 lg:mb-5' onClick={handleClick} style={ valuesError.fName === '' && valuesError.lName === '' &&  valuesError.username === '' && valuesError.studentNum === '' && valuesError.email === '' && valuesError.password === '' && valuesError.confPassword === '' ? {pointerEvents: "auto", opacity: "100%"} : {pointerEvents: "none", opacity: "50%"}}>JOIN US</button>
-                                <p className='text-sm text-blk text-center cursor-pointer' onClick={signLink}
+                                <p className='text-sm text-blk text-center cursor-pointer hover:underline' onClick={signLink}
                                 >I already have an account</p>
                             </div>
                             
