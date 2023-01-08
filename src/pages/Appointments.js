f00ce1cc218e5c6491a66dc7442c6d7b26bb881f
AJ Espinosa <espinosa.aaron45@gmail.com>
2023-01-09 01:50:10 +0800

    Init appointment page

--- src/pages/Appointments.js
+++ src/pages/Appointments.js
@@ -12,6 +12,11 @@ const Appointments = () => {
 	}, [])
 	return (
 		<>
+			<div className='bg-wht absolute top-0 w-full font-space'>
+				<div className='px-[1.25rem] pt-20 pb-10 lg:pl-[21.5rem] lg:pr-[1.5rem] xl:pl-[22.5rem] xl:pr-[3rem] lg:pt-40 lg:pb-10 z-10'>
+					
+				</div>
+			</div>
 		</>
 	)
 }
