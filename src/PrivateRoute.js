518ce6cdbf172fbded35976e487f2e94c65f293c
AJ Espinosa <espinosa.aaron45@gmail.com>
2023-01-09 20:39:37 +0800

    Added Private Route

---
+++ src/PrivateRoute.js
@@ -0,0 +1,10 @@
+function PrivateRoute ({component: Component, authed, ...rest}) {
+    return (
+      <Route
+        {...rest}
+        render={(props) => authed === true
+          ? <Component {...props} />
+          : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
+      />
+    )
+  }
