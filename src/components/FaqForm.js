38eaea222cf62afb10b94eb468f5786fe0a41a99
AJ Espinosa <espinosa.aaron45@gmail.com>
2023-01-11 00:58:55 +0800

    Fix FAQ

--- src/components/FaqForm.js
+++ src/components/FaqForm.js
@@ -22,7 +22,7 @@ const FaqForm = () => {
             setError('You must be logged in.')
             return
         }
-        
+
         const response = await fetch('/api/FAQs', {
             method: 'POST',
             body: JSON.stringify(faq),
@@ -35,7 +35,7 @@ const FaqForm = () => {
         const json = await response.json();
 
         if (!response.ok) {
-            setError('Please fill the empty fields')
+            setError('Please fill the empty fields.')
         }
         if (response.ok) {
             setTitle('')
@@ -63,7 +63,7 @@ const FaqForm = () => {
                     placeholder="Question Title"
                     onChange={(e) => setTitle(e.target.value)}
                     value={title}
-                    className= "p-[12px] border-blk border-[2px] rounded-[3px]"
+                    className="p-[12px] border-blk border-[2px] rounded-[3px]"
                 />
 
                 <textarea
@@ -71,16 +71,18 @@ const FaqForm = () => {
                     placeholder="Do you have something in mind?"
                     onChange={(e) => setContent(e.target.value)}
                     value={content}
-                    className= "resize-y p-[12px] border-blk border-[2px] rounded-[3px] h-[10rem] lg:h-full"
+                    className="resize-y p-[12px] border-blk border-[2px] rounded-[3px] h-[10rem] lg:h-full"
                 />
             </div>
 
             {/* BUTTONS */}
-            <div className='flex gap-x-[12px] mt-[12px] w-full justify-end'>
-                <button className='px-5 py-2 font-bold bg-wht border-[2px] border-blk text-blk rounded-[3px] hover:bg-light-lgry transition-all ease-in-out duration-[0.2s]' onClick={clear}>Clear</button>
-                <button className='px-5 py-2 font-bold bg-orng border-[2px] border-blk text-blk rounded-[3px] hover:bg-light-orng transition-all ease-in-out duration-[0.2s]' onClick={handleSubmit}>Post</button>
+            <div className='flex flex-row items-center'>
+                {error && <div className="error w-full mt-[12px]">{error}</div>}
+                <div className='flex gap-x-[12px] mt-[12px] w-full justify-end'>
+                    <button className='px-5 py-2 font-bold bg-wht border-[2px] border-blk text-blk rounded-[3px] hover:bg-light-lgry transition-all ease-in-out duration-[0.2s]' onClick={clear}>Clear</button>
+                    <button className='px-5 py-2 font-bold bg-orng border-[2px] border-blk text-blk rounded-[3px] hover:bg-light-orng transition-all ease-in-out duration-[0.2s]' onClick={handleSubmit}>Post</button>
+                </div>
             </div>
-            {error && <div className="error">{error}</div>}
         </div>
     )
 }
