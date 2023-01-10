1101cb39271a65265485e33ee9c1cba44312cbf2
AJ Espinosa <espinosa.aaron45@gmail.com>
2023-01-11 01:06:15 +0800

    Added comment to appointments for collab

--- src/components/AppCard.js
+++ src/components/AppCard.js
@@ -11,13 +11,26 @@ const AppCard = () => {
                 </div>
 
                 <div className='flex flex-col lg:flex-row bg-light-lgry border-l-orng border-l-[5px] w-full justify-start lg:justify-between items-start py-[3rem] pl-3 lg:pl-[24px] cursor-pointer'>
+
+                    {/* APPOINTMENT TYPE + APPOINTMENT TITLE */}
                     <p className='underline font-bold cursor-pointer hover:text-orng transition-all ease-in-out duration-[0.2s]'>DOCUMENT: Certificate of Grades</p>
+
                     <div className='flex flex-col lg:flex-row gap-x-2 lg:pr-[24px] font-bold select-none'>
+
+                        {/* APPOINTMENT DATE - PLACEHOLDER MUNA */}
                         <p>Schedule: 12/12/2022</p>
+
+                        {/* LINE */}
                         <p className='hidden lg:block'>|</p>
+
+                        {/* APPOINTMENT TIME - PLACEHOLDER MUNA */}
                         <p>10:00 AM</p>
                     </div>
+
                 </div>
+
+                {/* KAHIT DITO MO MUNA STORE YUNG APPOINTMENT DESCRIPTION - NASA LABAS TALAGA S'YA NG CARD AS OF NOW, AKO NA MAG-AYOS NG STYLES KAPAG NAFE-FETCH NA S'YA */}
+                <p></p>
 
             </div>
         </>
--- src/pages/Appointments.js
+++ src/pages/Appointments.js
@@ -38,15 +38,12 @@ const Appointments = () => {
 				<div className='px-[1.25rem] pt-20 pb-10 lg:pl-[21.5rem] lg:pr-[1.5rem] xl:pl-[22.5rem] xl:pr-[3rem] lg:pt-40 lg:pb-10 z-10'>
 					<div className='flex flex-col gap-y-4'>
 						{/* TITLE */}
-						<div className='flex flex-row justify-between items-center w-full'>
+						<div className='flex flex-row items-center w-full'>
 							<p className='text-orng font-bold text-lg lg:text-xl'>YOUR APPOINTMENTS</p>
-							<p className='text-[0.65rem] font-bold lg:text-sm text-blk'>See more</p>
 						</div>
 
 						{/* CARDS - DITO MA-STORE YUNG DATA FROM USER */}
 						<div className='flex flex-col gap-y-3'>
-							<AppCard />
-							<AppCard />
 							<AppCard />
 						</div>
 
