cd8129db3264336dc160fd769087669b5621fbb4
AJ Espinosa <espinosa.aaron45@gmail.com>
2023-01-10 17:10:22 +0800

    Failsafe: Fixing error message signup

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -3,16 +3,15 @@ import { useEffect } from 'react'
 
 const FormInput = (props) => {
 
-    const { error, empty, ...inputProps } = props
+    const { error, showError, empty, ...inputProps } = props
 
     useEffect(() => {
-        console.log(error)
       }, [error])
 
     return (
         <div className="relative">
-            <input {...inputProps} className='peer bg-white w-full h-[40px] border-[2px] border-blk rounded-[2px] font-space text-sm px-[12px] mb-5 outline-none cursor-text' autocomplete="off"></input>
-            <p className='absolute top-1 right-[17rem] w-full text-right text-sm'>{error}</p>
+            <input {...inputProps} className='bg-white w-full h-[40px] border-[2px] border-blk rounded-[2px] font-space text-sm px-[12px] mb-5 outline-none cursor-text' autocomplete="off" style={showError === true ? {borderColor: 'red'} : {borderColor: 'black'}}></input>
+            <p className='absolute top-1 right-[17rem] w-full text-right text-sm' style={showError === true ? {display: 'block'} : {display: 'none'}}>{error}</p>
         </div>
     )
 }
