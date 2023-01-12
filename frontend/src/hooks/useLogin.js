692fb4150d594e7cb22c7b3bfb9454819bff4977
AJ Espinosa <espinosa.aaron45@gmail.com>
2023-01-13 02:35:47 +0800

    fix api

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -10,7 +10,7 @@ export const useLogin = () => {
         setIsLoading(true);
         setError(null);
 
-        const response = await fetch('/api/user/login', {
+        const response = await fetch('https://wingman-app-api.vercel.app/api/user/login', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({ email, password })
