b9f139a744e25e55e7ce3be47cf0c3d5129a3ee1
AJ Espinosa <espinosa.aaron45@gmail.com>
2023-01-13 11:49:55 +0800

    fix api

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -10,7 +10,7 @@ export const useLogin = () => {
         setIsLoading(true);
         setError(null);
 
-        const response = await fetch('api/user/login', {
+        const response = await fetch('/api/user/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email, password })
