0ea0d763b46f6d2ddb6b921b80251b80cb54b4fe
AJ Espinosa <espinosa.aaron45@gmail.com>
2023-01-14 04:44:48 +0800

    Remake appointment cards

--- frontend/src/components/AppCard.js
+++ frontend/src/components/AppCard.js
@@ -2,39 +2,45 @@ import React, { useState } from 'react'
 
 const AppCard = (props) => {
 
-    const {appointment, index} = props
+    const { appointment, index } = props
 
 
     return (
         <>
             <div className='flex flex-row'>
 
-                <div className='flex justify-center items-center pr-3 lg:pr-5'>
+                <div className='flex justify-center items-center pr-3 lg:pr-5 w-[3rem]'>
                     {/* INDEX NUNG ENTRY, KUNG PANG-ILAN NA APPOINTMENT */}
-                    <p className='text-orng font-bold text-3xl'>0{index+1}</p>
+                    <p className='text-orng font-bold text-3xl'>0{index + 1}</p>
                 </div>
 
-                <div className='flex flex-col lg:flex-row bg-light-lgry border-l-orng border-l-[5px] w-full justify-start lg:justify-between items-start py-[3rem] pl-3 lg:pl-[24px] cursor-pointer'>
+                <div className='flex flex-col lg:flex-col bg-light-lgry border-r-blk border-r-[2px] border-l-orng border-l-[5px] border-t-blk border-t-[2px] border-b-blk border-b-[2px] w-full cursor-pointer'>
 
-                    {/* APPOINTMENT TYPE + APPOINTMENT TITLE */}
-                    <p className='underline font-bold cursor-pointer hover:text-orng transition-all ease-in-out duration-[0.2s]'>{appointment.title}</p>
+                    <div className='flex justify-between items-center w-full p-[12px]'>
+                        {/* APPOINTMENT TYPE + APPOINTMENT TITLE */}
+                        <p className='w-[50%] lg:w-auto underline font-bold cursor-pointer uppercase hover:text-orng transition-all ease-in-out duration-[0.2s]'>{appointment.title}</p>
 
-                    <div className='flex flex-col lg:flex-row gap-x-2 lg:pr-[24px] font-bold select-none'>
+                        <div className='w-[50%] lg:w-auto flex flex-col lg:flex-row gap-x-2 font-bold select-none'>
 
-                        {/* APPOINTMENT DATE - PLACEHOLDER MUNA */}
-                        <p>Status: {appointment.status}</p>
+                            {/* APPOINTMENT DATE - PLACEHOLDER MUNA */}
+                            <p>Type: {appointment.type}</p>
 
-                        {/* LINE */}
-                        <p className='hidden lg:block'>|</p>
+                            {/* LINE */}
+                            <p className='hidden lg:block'>|</p>
 
-                        {/* APPOINTMENT TIME - PLACEHOLDER MUNA */}
-                        <p>Type: {appointment.type}</p>
+                            {/* APPOINTMENT TIME - PLACEHOLDER MUNA */}
+                            <p>Status: {appointment.status}</p>
+                        </div>
+                    </div>
+
+                    {/* DESCRIPTION */}
+                    <div className='py-3 bg-wht p-[12px]'>
+                        <p>{appointment.description}</p>
                     </div>
 
                 </div>
 
-                {/* KAHIT DITO MO MUNA STORE YUNG APPOINTMENT DESCRIPTION - NASA LABAS TALAGA S'YA NG CARD AS OF NOW, AKO NA MAG-AYOS NG STYLES KAPAG NAFE-FETCH NA S'YA */}
-                <p>{appointment.description}</p>
+
 
             </div>
         </>
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,9 +4,9 @@ const Footer = () => {
 
     return (
         <>
-            <footer className='flex flex-col justify-center items-center lg:flex-row lg:place-content-between font-black text-sm bg-wht border-t-[2px] border-blk w-full lg:pl-[22.5rem] h-28 px-10'>
-                <p>Wingman 2023</p>
-                <div className='flex flex-col items-center lg:flex-row gap-x-5'>
+            <footer className='flex flex-col justify-center items-center lg:flex-row lg:place-content-between text-sm bg-wht border-t-[2px] border-blk w-full lg:pl-[22.5rem] h-auto lg:h-28 px-10'>
+                <p className='py-10 lg:py-0 font-bold'>Wingman 2023</p>
+                <div className='flex flex-col items-center lg:flex-row gap-x-5 pt-5 pb-10 lg:py-0'>
                     <p className='cursor-pointer hover:text-light-gry'>Terms and Conditions</p>
                     <p className='cursor-pointer hover:text-light-gry'>Privacy Policy</p>
                 </div>
