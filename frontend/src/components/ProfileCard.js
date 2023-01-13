d1e80895b9a79d8bf9b53445858e158ef7149e7c
AJ Espinosa <espinosa.aaron45@gmail.com>
2023-01-14 04:24:04 +0800

    Add animations dev page

--- frontend/src/components/ProfileCard.js
+++ frontend/src/components/ProfileCard.js
@@ -24,52 +24,52 @@ const ProfileCard = (props) => {
 
     return (
         <>
-            <div className='bg-wht border-blk border-[2px] rounded-[5px] text-blk flex flex-col justify-center items-center p-[24px] hover:drop-shadow-hoverShadow cursor-pointer transition-all ease-in-out duration-[0.1s]'>
+            <div className='bg-wht border-blk border-[2px] rounded-[5px] text-blk flex flex-col justify-center items-center px-[24px] py-[36px] w-80'>
 
                 {/* IMG */}
-                <div className='overflow-hidden rounded-full'>
+                <div className='overflow-hidden rounded-full w-60'>
                     <motion.img
                         whileHover={{ scale: 1.07 }}
                         src={img}
-                        className="w-48 h-48 rounded-full cursor-pointer m-5" />
+                        className="w-full rounded-full cursor-pointer" />
                 </div>
 
                 {/* NAME */}
                 <motion.p
                     whileHover={{ color: '#FC5F1C', scale: 1.05, y: -3, rotate: -1.5 }}
-                    className='font-bold text-2xl'>{name}</motion.p>
+                    className='font-bold text-xl text-center pt-3 select-none'>{name}</motion.p>
 
                 {/* TITLES */}
-                <div className='flex flex-col items-center text-sm'>
+                <div className='flex flex-col items-center text-sm text-center pb-5 select-none'>
                     <p>{role1}</p>
                     <p>{role2}</p>
                 </div>
 
                 {/* ICONS */}
-                <div className='flex flex-row text-2xl gap-x-3 py-5'>
+                <div className='flex flex-row text-2xl gap-x-3'>
 
                     {/* GITHUB ICON */}
                     <p
                         className='hover:text-orng transition-all ease-in-out duration-[0.2s]'
-                        style={ghState ? { display: 'block' } : { display: 'none' }}><a href={ghLink}>{ghIcon}</a>
+                        style={ghState ? { display: 'block' } : { display: 'none' }}><a href={ghLink} target="_blank">{ghIcon}</a>
                     </p>
 
                     {/* FACEBOOK ICON */}
                     <p
                         className='hover:text-orng transition-all ease-in-out duration-[0.2s]'
-                        style={fbState ? { display: 'block' } : { display: 'none' }}><a href={fbLink}>{fbIcon}</a>
+                        style={fbState ? { display: 'block' } : { display: 'none' }}><a href={fbLink} target="_blank">{fbIcon}</a>
                     </p>
 
                     {/* TWITTER ICON */}
                     <p
                         className='hover:text-orng transition-all ease-in-out duration-[0.2s]'
-                        style={twtrState ? { display: 'block' } : { display: 'none' }}><a href={twtrLink}>{twtrIcon}</a>
+                        style={twtrState ? { display: 'block' } : { display: 'none' }}><a href={twtrLink} target="_blank">{twtrIcon}</a>
                     </p>
 
                     {/* INSTAGRAM ICON */}
                     <p
                         className='hover:text-orng transition-all ease-in-out duration-[0.2s]'
-                        style={igState ? { display: 'block' } : { display: 'none' }}><a href={igLink}>{igIcon}</a>
+                        style={igState ? { display: 'block' } : { display: 'none' }}><a href={igLink} target="_blank">{igIcon}</a>
                     </p>
 
                 </div>
--- frontend/src/pages/Devs.js
+++ frontend/src/pages/Devs.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { motion } from 'framer-motion'
 import { HeroFooter, HeroNav, ProfileCard } from '../components'
 import { devs } from '../constants'
+import { GiVibratingBall } from "react-icons/gi";
 
 const Devs = () => {
 
@@ -27,18 +28,43 @@ const Devs = () => {
                 <div className='p-5 lg:py-10 lg:px-56 w-full'>
                     <div className='flex flex-col gap-x-2 text-blk'>
                         {/* TITLE */}
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ delay: 1 * 0.2 }}>
+                            <motion.p
+                                whileHover={{ color: '#FC5F1C', scale: 1.15, rotate: -1.5 }}
+                                className='font-bold text-7xl lg:text-9xl text-center w-full select-none'>The Devs</motion.p>
+                        </motion.div>
+
                         <motion.p
-                            whileHover={{ color: '#FC5F1C', scale: 1.2, rotate: -1.5 }}
-                            className='font-bold text-9xl text-center w-full select-none'>The Devs</motion.p>
-                        <p className='font-bold text-xl text-center w-full py-5 px-28'>The developers of this website are three 3rd Year BS Computer Science students from Cavite State University - Don Severino Delas Alas Campus. Wingman is developed as a completion requirement for Software Engineering II.</p>
-                        <p className='font-bold text-xl text-center w-full py-5 px-28'>The main goal of the website is to help students lessen the inconvenience experienced inside the university by creating an online platform where they can interact with their schoolfellows through conversations and transactions. Wingman empowers the students when expressing their grievances, whether academic or non-academic. It also aims to maintain the student's awareness of their environment by delivering an essential communique that explains what transpires within Cavite State University. In order to uphold truth, excellence, and service.</p>
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ delay: 2 * 0.2 }}
+                            className='font-bold lg:text-xl text-center w-full py-5 lg:px-28'>The developers of this website are three 3rd Year BS Computer Science students from Cavite State University - Don Severino Delas Alas Campus. Wingman is developed as a completion requirement for Software Engineering II.</motion.p>
+
                         <motion.p
-                            animate={{ y: [-10, 0, -10] }}
-                            transition={{ repeat: Infinity, duration: 1.5 }}
-                            className='font-bold text-orng text-xl py-20 select-none w-full text-center'>↓ KNOW MORE ABOUT THEM ↓</motion.p>
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ delay: 3 * 0.2 }}
+                            className='font-bold lg:text-xl text-center w-full py-5 lg:px-28'>The main goal of the website is to help students lessen the inconvenience experienced inside the university by creating an online platform where they can interact with their schoolfellows through conversations and transactions. Wingman empowers the students when expressing their grievances, whether academic or non-academic. It also aims to maintain the student's awareness of their environment by delivering an essential communique that explains what transpires within Cavite State University. In order to uphold <span className='text-orng font-bold hover:underline cursor-default'>truth, excellence, and service.</span></motion.p>
+
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ delay: 4 * 0.2 }}>
+                            <motion.p
+                                animate={{ y: [-10, 0, -10] }}
+                                transition={{ repeat: Infinity, duration: 1.5 }}
+                                className='font-bold text-orng text-xl py-12 lg:py-20 select-none w-full text-center'>↓ KNOW MORE ABOUT THEM ↓</motion.p>
+                        </motion.div>
 
                         {/* PROFILE CARDS */}
-                        <div className='flex flex-row w-full justify-center items-center gap-x-[24px] pb-[48px]'>
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ delay: 5 * 0.2 }}
+                            className='flex flex-col lg:flex-row w-full justify-center items-center gap-x-[12px] xl:gap-x-[24px] gap-y-[12px] pb-[48px]'>
                             {devs.map((dev) => (
                                 <div>
                                     <ProfileCard
@@ -62,8 +88,26 @@ const Devs = () => {
                                     />
                                 </div>
                             ))}
+                        </motion.div>
 
-                        </div>
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ delay: 6 * 0.2 }}
+                            className='w-full flex flex-col gap-y-5 items-center justify-center text-light-gry py-10 z-20'>
+                            <motion.div
+                                animate={{ rotate: -360 }}
+                                drag
+                                dragConstraints={{ top: -50, right: 50, bottom: 50, left: -50, }}
+                                transition={{ ease: "linear", duration: 1, repeat: Infinity }}
+                            >
+                                <GiVibratingBall className='text-6xl' />
+                            </motion.div>
+                            <motion.p
+                                drag
+                                dragConstraints={{ top: -50, right: 50, bottom: 50, left: -50, }}
+                                className='font-bold select-none'>~just a random svg rotating~</motion.p>
+                        </motion.div>
                     </div>
                 </div>
 
